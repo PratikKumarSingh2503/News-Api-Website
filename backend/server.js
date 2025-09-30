04985ac0265bfd4bab0690f1c3b18f80a8e433cb
PratikKumarSingh2503 <pratikkumarsingh819@gmail.com>
2025-09-30 10:42:37 +0530

    first commit

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ app.use(cors());
 app.use(express.json());
 
 // MongoDB connection
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log("✅ MongoDB connected"))
     .catch(err => console.error(err));
 
